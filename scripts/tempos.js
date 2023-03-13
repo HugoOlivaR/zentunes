793ce6e820d3f03c2d1a7bf938ca95ce6334026b
Hugo Oliva <74360087+HugoOlivaR@users.noreply.github.com>
2023-03-13 20:26:01 +0100

    Update tempos.js

--- scripts/tempos.js
+++ scripts/tempos.js
@@ -25,6 +25,8 @@ setInterval(reloj, 1000)
 
 
 
+// CRONO
+
 var botonCrono = document.getElementById("botonCrono")
 var div_crono = document.getElementById("div_crono")
 var div_crono_encendido = false
@@ -46,7 +48,6 @@ botonCrono.addEventListener("click", function aparecer_boton(){
     }
 
 })
-
 
 
 var inicio_crono = document.getElementById("inicio_crono")
@@ -73,7 +74,6 @@ reinicio_crono.addEventListener("click", function aviso(){
 })
 
 
-//CRONO
 
 let segundos_crono = 0;
 let minutos_crono = 0;
@@ -116,3 +116,117 @@ document.getElementById("reinicio_crono").addEventListener("click", function () 
   running = false;
   document.getElementById("texto_crono").textContent = "00:00:00";
 });
+
+
+
+//TEMPORIZADOR
+
+let endTempo = document.getElementById("endTempo");
+
+
+var botontempo = document.getElementById("botonTempo")
+var div_tempo = document.getElementById("div_tempo")
+var div_tempo_encendido = false
+
+botontempo.addEventListener("click", function aparecer_boton(){
+
+    if(div_tempo_encendido == false){
+        div_tempo.style.display = "inline"
+        setTimeout(function() {
+            div_tempo.style.opacity = "1"
+        }, 50);
+        div_tempo_encendido = true
+    }else{
+        div_tempo.style.opacity = "0"
+        setTimeout(function() {
+            div_tempo.style.display = "none"
+        }, 500);
+        div_tempo_encendido = false
+    }
+
+})
+
+
+
+let segundos = 0;
+let temporizador;
+let botonIniciarDetener = document.getElementById("inicio_tempo");
+
+function actualizarTemporizador() {
+    let horas = Math.floor(segundos / 3600);
+    let minutos = Math.floor((segundos - (horas * 3600)) / 60);
+    let segundosMostrar = segundos % 60;
+    document.getElementById("texto_tempo").innerHTML = agregarCeros(horas) + ":" + agregarCeros(minutos) + ":" + agregarCeros(segundosMostrar);
+
+    
+}
+
+function iniciarDetenerTemporizador() {
+    if (temporizador) {
+        detenerTemporizador();
+        document.getElementById("img_inicio_tempo").setAttribute('src', 'src/img/play2.png')
+    } else {
+        temporizador = setInterval(function() {
+            if (segundos > 0) {
+                segundos--;
+                actualizarTemporizador();
+            } else {
+                detenerTemporizador();
+                document.getElementById("img_inicio_tempo").setAttribute('src', 'src/img/play2.png')
+                endTempo.play();
+                alert("¡Tiempo Agotado!")
+            }
+        }, 1000);
+        document.getElementById("img_inicio_tempo").setAttribute('src', 'src/img/pausa2.png')
+    }
+}
+
+function detenerTemporizador() {
+    clearInterval(temporizador);
+    temporizador = undefined;
+}
+
+function reiniciarTemporizador() {
+    detenerTemporizador();
+    segundos = 0;
+    actualizarTemporizador();
+    document.getElementById("img_inicio_tempo").setAttribute('src', 'src/img/play2.png')
+}
+
+function sumarUnMinuto() {
+    segundos += 60;
+    actualizarTemporizador();
+}
+
+function restarUnMinuto() {
+    if (segundos >= 60) {
+        segundos -= 60;
+        actualizarTemporizador();
+    }
+}
+
+function sumarDiezMinutos() {
+    segundos += 600;
+    actualizarTemporizador();
+}
+
+function restarDiezMinutos() {
+    if (segundos >= 600) {
+        segundos -= 600;
+        actualizarTemporizador();
+    }
+}
+
+function agregarCeros(numero) {
+    if (numero < 10) {
+        return "0" + numero;
+    }
+    return numero;
+}
+
+document.getElementById("sumar-1").addEventListener("click", sumarUnMinuto);
+document.getElementById("restar-1").addEventListener("click", restarUnMinuto);
+document.getElementById("sumar-10").addEventListener("click", sumarDiezMinutos);
+document.getElementById("restar-10").addEventListener("click", restarDiezMinutos);
+botonIniciarDetener.addEventListener("click", iniciarDetenerTemporizador);
+document.getElementById("reinicio_tempo").addEventListener("click", reiniciarTemporizador);
